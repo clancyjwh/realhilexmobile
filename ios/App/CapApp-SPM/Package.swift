afe19dbd65354ab8652b61b361dbf5c3e126fb0c
clancyjwh <clancyjhodgins@gmail.com>
2026-05-14 02:37:18 +0000

    Switch to official OneSignal Capacitor plugin

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -11,16 +11,14 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.4"),
-        .package(name: "OnesignalCordovaPlugin", path: "../../capacitor-cordova-ios-plugins/sources/OnesignalCordovaPlugin")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.4")
     ],
     targets: [
         .target(
             name: "CapApp-SPM",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "OnesignalCordovaPlugin", package: "OnesignalCordovaPlugin")
+                .product(name: "Cordova", package: "capacitor-swift-pm")
             ]
         )
     ]
